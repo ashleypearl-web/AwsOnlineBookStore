f5b5346cc53b83451668c7e0980cdc726266832b
Ashley Pearl <tamfuhashley@gmail.com>
2024-10-21 10:50:48 -0400

    Delete route53.tf

--- route53.tf
+++
@@ -1,9 +0,0 @@
-
-/* this is created by AWS external to Terraform and will be imported */
-resource "aws_route53_zone" "main" {
-  name = var.domain
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
